225e9dd6063c42ab26df723f53351c685b6f4f42
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-11-29 09:16:49 +0100

    Done es 3 of first module

---
+++ Modulo1/Calculator/Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter the first number..");
+
+string textA = Console.ReadLine();
+
+int numberA = int.Parse(textA);
+
+Console.WriteLine("Choose the operator");
+
+string op=Console.ReadLine();
+
+if (op.Length != 1)
+{
+    Console.WriteLine("Invalid input, try again later.");
+    Environment.Exit(1);
+    return;
+}
+
+Console.WriteLine("Please enter the second number..");
+
+string textB = Console.ReadLine();  
+
+int numberB = int.Parse(textB);
+
+int result;
+
+
+
+
+char operatorChar = op.First();
+
+switch(operatorChar)
+{
+    case '+':
+        result = numberA + numberB;
+        break;
+    case '-':
+        result = numberA - numberB;
+        break;
+    case '*':
+        result = numberA * numberB;
+        break;
+    case '/':
+        result = numberA / numberB;
+        break;
+    default:
+        Console.WriteLine($"Invalid operator: {operatorChar}, try again later");
+        Environment.Exit(1);
+        return;
+}
+
+Console.Write($"{numberA} {op} {numberB} = ");
+Console.WriteLine(result);
