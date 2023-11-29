357dbecb1bc226dc8104795073b58248c13a387d
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-11-29 09:38:21 +0100

    Done es 4 of first module

---
+++ Modulo1/Text Manipulation/Text Manipulation/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Please enter some text");
+
+string input = Console.ReadLine();
+
+string reversed = string.Empty;
+
+
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    reversed += input[i];
+}
+Console.Write($"Reverse String is : {reversed} ");
