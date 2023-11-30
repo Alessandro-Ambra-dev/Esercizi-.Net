4d268c6ccf0181a24bf545e69563c89867bbcae9
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-11-30 19:37:51 +0100

    Done es 6 of Inheritance

---
+++ Modulo1/Inheritance/Inheritance/Animals.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public abstract class Animals
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public abstract void Speak();
+
+        internal class Dog : Animals 
+        {
+           public override void Speak()
+            {
+                Console.WriteLine($"{Name} is barking and is {Age} years old");
+            }
+        }
+
+        internal class Cat : Animals
+        {
+            public override void Speak()
+            {
+                Console.WriteLine($"{Name} is meowing and is {Age} years old");
+            }
+        }
+        internal class Cow : Animals
+        {
+            public override void Speak()
+            {
+                Console.WriteLine($"{Name} is mooing and is {Age} years old");
+            }
+        }
+        internal class Leon : Animals
+        {
+            public override void Speak()
+            {
+                Console.WriteLine($"{Name} is roaring and is {Age} years old");
+            }
+        }
+    }
+}
---
+++ Modulo1/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Inheritance;
+using static Inheritance.Animals;
+
+Animals dog = new Animals.Dog
+{
+    Name = "Fuffi",
+    Age = 7,
+};
+dog.Speak();
+
+Animals cat = new Animals.Cat
+{
+    Name = "Lala",
+    Age = 8,
+};
+cat.Speak();
+
+Animals cow = new Animals.Cow
+{
+    Name = "Mumi",
+    Age = 9,
+};
+cow.Speak();
+
+Animals leon = new Animals.Leon
+{
+    Name = "Rocco",
+    Age = 10,
+};
+leon.Speak();
