e72236a17973c528d5dfeefbd904008e2f4462cd
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-12-03 20:37:30 +0100

    Done es 5 of Module 1

---
+++ Modulo1/Reading from a text/ReadingFromAText/ReadingFromAText/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Please put here some greetings:");
+
+
+string response =  Console.ReadLine();
+
+var path = "./output.txt";
+
+var path2 = "./output.ppt";
+
+File.WriteAllText(path2, response);
+
+
+
+
+File.AppendAllLines(path, new[]{ response });
+File.AppendAllLines(path2, new[]{ response });
+
+
+var text = File.ReadAllText(path);
+var text2 = File.ReadAllText(path2);
+
+Console.WriteLine(text);
+Console.WriteLine(text2);
