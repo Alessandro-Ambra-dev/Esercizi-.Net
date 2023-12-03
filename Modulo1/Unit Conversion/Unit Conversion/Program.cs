f0d45f5a84bbcbf5b37fd1b8dd5ea9cba3a0d4d1
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-12-03 22:01:50 +0100

    Done es 9 of Module 1

---
+++ Modulo1/Unit Conversion/Unit Conversion/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please insert a degree in Celsius");
+
+string input = Console.ReadLine();
+
+double celsius = int.Parse(input);
+
+Console.WriteLine("Converting in Fahrenhite...");
+
+double far = (celsius * 1.8d) + 32;
+
+Console.WriteLine(far);
