5990b58ea37fd0dc1b9f4183183c9829d0616cf9
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-12-03 21:08:55 +0100

    Done es 7 of Module 1

---
+++ Modulo1/Access Denied/Access Denied/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Animal
+{
+    internal string Name { get; set; }
+    public string Description {internal set; get; }
+    public int Age { get; set; }
+
+    internal void Call()
+    {
+        Console.WriteLine($"{Name} come here!");
+    }
+    internal void Describe()
+    { 
+        Console.WriteLine($"{Name} is {Description}");
+
+
+    }
+}
---
+++ Modulo1/Access Denied/Access Denied/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Animal dog = new Animal
+{
+    Age = 10,
+    Name = "fuffi",
+    Description = " a malaysian dog, he's 10 year, he's a rare and vicious breed"
+};
+
+Animal cat = new()
+{
+    Name = "SnowBall",
+    Description = " a 5 years old kitty cat with some lovely stripes",
+    Age = 15
+};
+
+cat.Call();
+
+dog.Call();
+
+cat.Describe();
+
+dog.Describe(); 
+
+
+
+
+
