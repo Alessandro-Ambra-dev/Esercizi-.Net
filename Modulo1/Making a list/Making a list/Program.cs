4eec6dc6b9f0462ec48a769587b4137d9ad21e11
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-12-07 12:46:11 +0100

    Done es 1 of Module 2

---
+++ Modulo1/Making a list/Making a list/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Hello, write your first list");
+
+
+
+string[] input = new string[10];
+
+string[] revered = input;
+
+for(int i = 0; i < input.Length; i++)
+{
+    input[i] = Console.ReadLine();
+}
+
+Console.WriteLine("The original Array is:");
+
+foreach (var s in input)
+{
+    Console.WriteLine(s);
+}
+
+
+
+Array.Reverse(revered);
+
+Console.WriteLine("The reversed Array is:");
+
+foreach(var s in revered)
+{
+    Console.WriteLine(s);
+}
+
+
+
+
