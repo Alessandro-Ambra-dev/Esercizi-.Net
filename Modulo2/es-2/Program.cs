cb10578ffa7e56f973da4cd15ca4ac3151475479
Elisa Farinella <elisafarinella3@MacBook-Air-di-Elisa.local>
2023-12-16 19:14:33 +0100

    added es 2 of Module2

---
+++ Modulo2/es-2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+
+Console.WriteLine("This is an ArrayList");
+
+
+ArrayList MyList = new ArrayList();
+
+MyList.Add("hello");
+MyList.Add(123);
+MyList.Add(true);
+MyList.Add(3.5);
+
+foreach (var item in MyList)
+{
+    Console.WriteLine(item);
+}
