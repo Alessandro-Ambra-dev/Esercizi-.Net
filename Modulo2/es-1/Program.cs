bcd5db53c30804cbd1c6f7f716df1fa511d578da
Elisa Farinella <elisafarinella3@MacBook-Air-di-Elisa.local>
2023-12-16 18:57:44 +0100

    added es 1 of Lists

---
+++ Modulo2/es-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write some text:");
+
+var Words = new List<string>();
+
+for (int i = 0; i < 10; i++)
+{
+    var item = Console.ReadLine();
+    Words.Add(item);
+}
+
+Console.WriteLine("The list of words is: ");
+foreach (var word in Words)
+{
+    Console.WriteLine(word);
+}
+
+Console.WriteLine("The reversed list of words is: ");
+
+for (int i =(Words.Count -1); i>=0; i-- )
+{
+    var item= Words[i];
+   Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+
